5c2c6a146a4e65164625800ed38120961035b436
Chingis <cmontayev@gmail.com>
2020-11-15 01:19:54 +0600

    Создание таблицы для User(ов)  Удаление таблицы User(ов)  Очистка содержания таблицы  Добавление User в таблицу  Удаление User из таблицы ( по id )  Получение всех User(ов) из таблицы

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,15 +1,34 @@
 package jm.task.core.jdbc;
 
 
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
         UserServiceImpl userService = new UserServiceImpl();
         userService.cleanUsersTable();
+         User user1 = new User("Daneen","Seisenbay", (byte) 7);
+         User user2 = new User("Chingis","Montay", (byte) 32);
+         User user3 = new User("Zarina","Dosmail", (byte) 30);
+         User user4 = new User("Alaya","Seisenbay", (byte) 5);
+        List<User>userList = Arrays.asList(user1,user2,user3,user4);
+
+        for (User user : userList) {
+            userService.saveUser(user.getName(),user.getLastName(),user.getAge());
+        }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+        Util.closeConnection();
+
+
+
     }
 }
