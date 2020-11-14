e5bf5202435e520c776b75e0200ff4c9f1b02cb0
Chingis <cmontayev@gmail.com>
2020-11-15 01:01:24 +0600

    createUsersTable dropUserTable saveUser cleanTable removeId

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,15 @@
 package jm.task.core.jdbc;
 
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.cleanUsersTable();
     }
 }
